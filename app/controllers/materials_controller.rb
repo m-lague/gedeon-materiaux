574dad665a7adab8f75fc6c984180f0744480cf9
Mathieu Lague <mathieu@lague.me>
2024-02-05 22:57:25 +0100

    generation controllers & views

---
+++ app/controllers/materials_controller.rb
@@ -0,0 +1,49 @@
+class MaterialsController < ApplicationController
+  before_action :set_material, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @materials = Material.all
+  end
+
+  def show
+  end
+
+  def new
+    @material = Material.new
+  end
+
+  def create
+    @material = Material.new(material_params)
+    if @material.save
+      redirect_to @material, notice: 'Material was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @material.update(material_params)
+      redirect_to @material, notice: 'Material was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @material.destroy
+    redirect_to materials_url, notice: 'Material was successfully destroyed.'
+  end
+
+  private
+
+  def set_material
+    @material = Material.find(params[:id])
+  end
+
+  def material_params
+    params.require(:material).permit(:name, :description)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,25 @@
 Rails.application.routes.draw do
+  get 'ratings/index'
+  get 'ratings/show'
+  get 'ratings/new'
+  get 'ratings/create'
+  get 'ratings/edit'
+  get 'ratings/update'
+  get 'ratings/destroy'
+  get 'suppliers/index'
+  get 'suppliers/show'
+  get 'suppliers/new'
+  get 'suppliers/create'
+  get 'suppliers/edit'
+  get 'suppliers/update'
+  get 'suppliers/destroy'
+  get 'materials/index'
+  get 'materials/show'
+  get 'materials/new'
+  get 'materials/create'
+  get 'materials/edit'
+  get 'materials/update'
+  get 'materials/destroy'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
