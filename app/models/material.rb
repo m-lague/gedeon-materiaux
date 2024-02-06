6c228ab48d43c713b062402a223536a7a8abbbf7
Mathieu Lague <mathieu@lague.me>
2024-02-06 15:16:41 +0100

    materials#index without auth

--- app/models/material.rb
+++ app/models/material.rb
@@ -1,9 +1,9 @@
 class Material < ApplicationRecord
+
   has_many :ratings, dependent: :destroy
   has_many :material_suppliers, dependent: :destroy
   has_many :suppliers, through: :material_suppliers
 
-  validates :user_id, uniqueness: { scope: :material_id, message: "Vous avez déjà voté pour ce matériau" }
 
   
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :ratings, dependent: :destroy
+ 
 
-  enum :role, { admin: 0, free_user: 1, paying_user: 2}
+  enum :role, { admin: 0, free_user: 1, paying_user: 2 }
 end
