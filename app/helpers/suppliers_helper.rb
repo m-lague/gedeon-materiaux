8ed4806569b71d946f330555820111aeef5c90f0
Mathieu Lague <mathieu@lague.me>
2024-02-09 14:17:31 +0100

    fix supplier helper links

--- app/helpers/suppliers_helper.rb
+++ app/helpers/suppliers_helper.rb
@@ -1,9 +1,9 @@
 module SuppliersHelper
-  def link_destroy(supplier)
+  def link_destroy_supplier(supplier)
     link_to "Supprimer", supplier, method: :delete, data: { confirm: "Êtes-vous sûr?" }, class: "btn btn-danger" if policy(supplier).destroy?
   end
 
-  def link_edit(supplier)
+  def link_edit_supplier(supplier)
     link_to "Modifier", edit_supplier_path(supplier), class: "btn btn-outline-warning" if policy(supplier).update?
   end
 end
