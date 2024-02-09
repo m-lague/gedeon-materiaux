8f7dc6a8f933df1380442ec3bcfc9f039157013d
Mathieu Lague <mathieu@lague.me>
2024-02-09 14:05:38 +0100

    try fix 500 in production at user/signin

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,17 +67,17 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address: 'gedeon.guide',
-    port: 465,
-    domain: 'gedeon.guide', # Remplacez par votre propre domaine
-    user_name: ENV['MAIL_USERNAME'],
-    password: ENV['MAIL_PASSWORD'],
-    authentication: :plain,
-    enable_starttls_auto: true
-}
+  config.action_mailer.raise_delivery_errors = false
+#   config.action_mailer.delivery_method = :smtp
+#   config.action_mailer.smtp_settings = {
+#     address: 'gedeon.guide',
+#     port: 465,
+#     domain: 'gedeon.guide', # Remplacez par votre propre domaine
+#     user_name: ENV['MAIL_USERNAME'],
+#     password: ENV['MAIL_PASSWORD'],
+#     authentication: :plain,
+#     enable_starttls_auto: true
+# }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
