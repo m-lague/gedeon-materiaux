49f163d2cb65e8db21b7b27303b1b18ba056cb1c
Mathieu Lague <mathieu@lague.me>
2024-03-17 10:46:57 +0100

    fix turbo&devise when current_user.nil?

--- app/helpers/upvotes_helper.rb
+++ app/helpers/upvotes_helper.rb
@@ -1,3 +1,10 @@
 module UpvotesHelper
 
+  def upvote_button(item)
+    if current_user
+      button_to item.upvotes.count, toggle_vote_path(item_id: item.id, item_type: item.class.to_s)
+    else
+      button_to item.upvotes.count, toggle_vote_path(item_id: item.id, item_type: item.class.to_s), data: {turbo: false}
+    end
+  end
 end
